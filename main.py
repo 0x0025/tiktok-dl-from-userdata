a5a5e62e30036832535c10d78a807eee24098ae9
0x25 <40762411+0x0025@users.noreply.github.com>
2022-03-12 15:56:57 +0300

    redirect link working

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import re
+from urllib import request
 import yt_dlp
+import requests
+
 
 def dl(url):
+    #print('Getting download url...')
+    #r = requests.get(url) 
+    #print(f'Downloading video: {r.url}')
     ydl_opts = {
         'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
         'ignoreerrors': True,
@@ -8,5 +15,5 @@ def dl(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-dl("https://www.tiktok.com/@wemijs/video/7069031542881848578?")
-#https://www.tiktokv.com/share/video/7069031542881848578/
+#dl("https://www.tiktok.com/@wemijs/video/7069031542881848578?")
+dl("https://www.tiktokv.com/share/video/7069031542881848578/")
